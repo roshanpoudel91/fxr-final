aaf6577a9600381cd57ed376835ce47f530743fd
Amit Rajbhandari <amit.fist@gmail.com>
2022-05-17 22:43:55 +0545

    completion of main page UI with responsiveness

--- resources/views/add-page.blade.php
+++ resources/views/add-page.blade.php
@@ -1,20 +1,38 @@
 <html>
-    <head>
-        <title>
-             Page Add
-        </title>
-        <link rel="stylesheet" href="{{ URL::asset('shopify.css') }}">
-       
-    </head>
 
-    <body>
-        <form action="/page/save" method="POST">
+<head>
+    <title>
+        Page Add
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('shopify.css') }}">
+</head>
+
+<body>
+    <form action="/page/save" method="POST">
         @csrf
-        <h1>Add Page</h1></br>
-        <input type="text" name="title" placeholder="Title"/></br></br>
-        <!-- <input type="text" name="body_html" placeholder="HTML Body"/></br></br></br> -->
-        @include('html_body')
-        <button class="save-button" type="submit">Save</button>
-        </form>
-    </body>
+        <main class="main-wrapper">
+            <div class="container">
+                <header class="main-header">
+                    <img class="main-logo" src="{{ URL::asset('fxr-site-logo-svg.svg') }}" alt="FXR" />
+                </header>
+
+                <div class="page-field-wrap">
+                    <input type="text" name="title" placeholder="Enter Page Title" class="page-title-field" />
+                    <img src="{{ URL::asset('icon-edit.svg') }}" alt="icon" />
+                </div>
+
+                <!-- <input type="text" name="body_html" placeholder="HTML Body"/></br></br></br> -->
+                @include('html_body')
+            </div>
+        </main>
+    </form>
+</body>
+
 </html>
--- resources/views/html_body.blade.php
+++ resources/views/html_body.blade.php
@@ -1,67 +1,80 @@
-<img src="{{ URL::asset('sample-banner.jpg') }}" alt="Boy on bike">
+<figure class="fxr-banner">
+    <picture>
+        <source media="(min-width:768px)" srcset="{{ URL::asset('sample-banner.jpg') }}">
+        <img src="{{ URL::asset('sample-banner-mobile.jpg') }}" alt="Flowers" style="width:auto;">
+    </picture>
+    <!--
+    <img src="{{ URL::asset('sample-banner.jpg') }}" srcset="{{ URL::asset('sample-banner-mobile.jpg') }} 800w,
+        {{ URL::asset('sample-banner.jpg') }} 1600w" alt="Boy on bike"> -->
+</figure>
 
-<h1>rpoudel91@gmail.com</h1>
+<h1 class="main-title">rpoudel91@gmail.com</h1>
 
-<p id="sampleText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<p id="sampleText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it
+    to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+    containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
+    versions of Lorem Ipsum.</p>
 
-<p id="count"></p>
-<button type="button" onclick="startCount()" id="javascript">JavaScript!</button>
+<footer class="main-footer">
+    <button class="btn-primary" type="submit">Save</button>
+    <button type=" button" onclick="startCount()" id="javascript" class="btn-secondary">JavaScript!</button>
+    <p id="count"></p>
+</footer>
 
 <script>
-    function startCount (){
+function startCount() {
     let futureTime = 10;
 
-    if(document.getElementById("javascript").innerHTML == "JavaScript!"){
+    if (document.getElementById("javascript").innerHTML == "JavaScript!") {
 
-            var x = setInterval(function() {
+        var x = setInterval(function() {
 
             console.log(futureTime);
             document.getElementById("count").innerHTML = futureTime;
             futureTime = futureTime - 1;
-                
+
             if (futureTime == -1) {
                 clearInterval(x);
                 //change every letter L and l to *
                 var oldStr = document.getElementById("sampleText").innerHTML;
                 console.log(oldStr);
-                setCookie("oldStr",oldStr,1);
+                setCookie("oldStr", oldStr, 1);
                 var newStr = oldStr.replace(/[l|L]/g, '<span style="color:red;">*</span>');
                 document.getElementById("sampleText").innerHTML = newStr;
                 document.getElementById("javascript").innerHTML = "Reset";
             }
-            }, 1000);
+        }, 1000);
 
-    }else{
+    } else {
         document.getElementById("sampleText").innerHTML = getCookie("oldStr");
         document.getElementById("javascript").innerHTML = "JavaScript!";
     }
 
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-
-    function setCookie(cname, cvalue, exdays) {
-            const d = new Date();
-            d.setTime(d.getTime() + (exdays*24*60*60*1000));
-            let expires = "expires="+ d.toUTCString();
-            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
-
-
-    function getCookie(cname) {
-            let name = cname + "=";
-            let decodedCookie = decodeURIComponent(document.cookie);
-            let ca = decodedCookie.split(';');
-            for(let i = 0; i <ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-                }
-                if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-                }
-            }
-            return "";
-    }
-
-
-        </script>
-
+    return "";
+}
+</script>
